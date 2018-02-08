4515ca413478015e832dbd71ac2f9fc54b4f1804
picce <picce@yahoo.it>
2018-02-08 12:29:13 +0100

    initial deploy and version 1.0.2

---
+++ L10n.cs
@@ -0,0 +1,72 @@
+﻿using System.Globalization;
+using Telerik.Sitefinity.DynamicModules.Model;
+using Telerik.Sitefinity.Model;
+using Telerik.Sitefinity.Localization;
+
+
+namespace Pigeonlabs.Sitefinity
+{
+    /// <summary>
+    /// helper for sitefinity localization
+    /// ideal for api l10n
+    /// </summary>
+    public class L10n
+    {
+        public string Lang { get; } = "";
+
+        public CultureInfo Culture { get; } = null;
+
+        public CultureInfo CultureDefault
+        {
+            get
+            {
+                return Res.SiteCultures[0];
+            }
+        }
+
+        /// <param name="lang">lang could be from Headers?.AcceptLanguage or from qry param</param>
+        public L10n(string lang = "")
+        {
+            this.Lang = lang;
+            try
+            {
+                this.Culture = new CultureInfo(lang);
+            }
+            catch { }
+        }
+
+        /// <summary>
+        /// wrapper around Telerik.Sitefinity.Localization.Res.TryGet() method
+        /// </summary>
+        public string GetRes(string classId, string key, string defaultValue = "")
+        {
+            string res = "";
+            if (!Res.TryGet(classId, key, Culture, out res))
+            {
+                res = defaultValue;
+            }
+            return res;
+        }
+
+        /// <summary>
+        /// wrapper around Telerik.Sitefinity.Model.DataExtensions.GetString() method
+        /// </summary>
+        public string DynString(DynamicContent source, string fieldName, string defaultValue = "")
+        {
+            string res = "";
+
+            //check for right value
+            source.GetString(fieldName).TryGetValue(out res, Culture);
+            
+            //fallback on first culture available
+            if (res == null)
+                source.GetString(fieldName).TryGetValue(out res, CultureDefault);
+            
+            //fallback on default value
+            if (res == null)
+                res = defaultValue;
+
+            return res;
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Pigeonlabs.Sitefinity.L10n")]
+[assembly: AssemblyDescription("Helper class for sitefinity localization, ideal for api l10n")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Pigeonlabs.Sitefinity.L10n")]
+[assembly: AssemblyCopyright("Copyright ©  2018 - Pigeonlabs")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("154a98de-9b60-4b9a-b6c1-a7e23ac15683")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.2.0")]
+[assembly: AssemblyFileVersion("1.0.2.0")]
